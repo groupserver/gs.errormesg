a3cddfeaed29eea74bbb439926c555c6ae6941c4
richard@onlinegroups.net <richard@onlinegroups.net>
2012-08-06 13:46:28 +1200

    Implement Forbidden error

--- gs/errormesg/forbidden.py
+++ gs/errormesg/forbidden.py
@@ -1,14 +1,31 @@
 from five import grok
+from zope.publisher.interfaces import IForbidden
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from urllib import quote
+
 from baseerror import BaseError
-from zope.security.interfaces import IForbidden
 
-class Forbidden(grok.View):
-    grok.name('error.html')
+class Forbidden(BaseError, grok.View):
+    grok.name('forbidden.html')
     grok.context(IForbidden)
+    index = ZopeTwoPageTemplateFile('browser/templates/forbidden.pt')
+    status = 403
 
-    def update(self):
-        self.response.setStatus(403)
+    def supportMessage(self):
+        m = u'''Hi! I saw a Forbidden (403) page when I went to
+%(url)s
 
-    def render(self):
-        return HTML_TEMPLATE % (
-            self.__class__.__name__, str(self.context.error))
+I expected to see...
+
+
+-----
+Technical details:
+Code: 403
+URL: %(url)s
+Referer: %(referer)s
+''' % {'url': self.errorUrl,
+       'referer': self.refererUrl}
+
+        retval = quote(m)
+        
+        return retval
