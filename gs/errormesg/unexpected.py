9dc836111d03e907eb70bf1a686b115c2412cd15
Michael JasonSmith <mpj17@onlinegroups.net>
2012-11-06 17:03:22 +1100

    Updating the Zope 2 Unexpected Error page, so it works.

--- gs/errormesg/unexpected.py
+++ gs/errormesg/unexpected.py
@@ -3,8 +3,18 @@ import traceback
 from urllib import quote
 from five import grok
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
-from baseerror import BaseError
+from baseerror import BaseError, BaseErrorPage
 
+MESSAGE = u'''Hi! I saw a Server Error (500) page when I went to 
+{url}
+
+I want to see...
+
+--
+This technical information may help you fix the error:
+
+{message}
+'''
 
 class UnexpectedError(BaseError, grok.View):
     grok.name('error.html')
@@ -19,17 +29,31 @@ class UnexpectedError(BaseError, grok.View):
         return formatted_tb.splitlines()[-1].strip()
 
     def supportMessage(self):
-        message = self.tracebackMessage()
-        m = u'Hi! I saw a Server Error (500) page when I went to '\
-            u'\n%s\n\nI want to see...\n\n--\n\nThis technical '\
-            u'information may help you fix the error:\n\n%s' % \
-            (self.errorUrl, message)
+        tracebackMessage = self.tracebackMessage()
+        m = MESSAGE.format(url=self.errorUrl, message=tracebackMessage)
         retval = quote(m)
-
         return retval
 
     def update(self):
         self.response.setStatus(500)
 
-    #def render(self):
-    #    return self.index(self.context)
+
+class UnexpectedZope2(BaseErrorPage):
+    '''The Zope2 version of the error page.'''
+    status = 500
+
+    def __init__(self, context, request):
+        super(UnexpectedZope2, self).__init__(context, request)
+        self.errorUrl = self.requested = request.form.get('q', '')
+
+        self.traceback = request.form.get('m', '')
+        self.tracebackMessage = self.traceback.strip().split('\n')[-1]
+
+        contentType = 'text/html; charset=UTF-8'
+        self.request.response.setHeader('Content-Type', contentType)
+        self.request.response.setStatus(self.status, lock=True)
+        
+    def supportMessage(self):
+        m = MESSAGE.format(url=self.requested, message=self.traceback)
+        retval = quote(m)
+        return retval
