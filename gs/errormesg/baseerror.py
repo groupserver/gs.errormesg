9846c38d2c06c873c655bc41736cca3d3567bb58
Michael JasonSmith <mpj17@onlinegroups.net>
2012-11-06 15:56:09 +1100

    An attempt to re-create the old error pages.

--- gs/errormesg/baseerror.py
+++ gs/errormesg/baseerror.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 *-*
 import re
 from urlparse import urlparse
+from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from Products.Five import BrowserView
 from Products.XWFCore.XWFUtils import get_support_email
 
 
-class BaseError(object):
+class CoreError(object):
+    '''The abstract base class for error messages.'''
+
+    status = None  # must be set in subclass
+
     searchEngines = [
         re.compile('.*search\.yahoo\.com'),
         re.compile('www.google\.com'),
         re.compile('www.bing.com'),
     ]
 
-    status = None  # must be set in subclass
-
-    @property
+    @Lazy
     def siteInfo(self):
-        siteInfo = createObject('groupserver.SiteInfo',
-                                self.context)
-        return siteInfo
+        retval = createObject('groupserver.SiteInfo', self.context)
+        return retval
 
-    @property
+    @Lazy
     def supportEmail(self):
         # we pretty much ignore errors. There is bugger all we can do
         # about an error in a error
@@ -30,7 +32,7 @@ class BaseError(object):
         except:
             return 'support@'
 
-    @property
+    @Lazy
     def refererUrl(self):
         # we pretty much ignore errors. There is bugger all we can do
         # about an error in a error
@@ -39,34 +41,31 @@ class BaseError(object):
         except:
             return 'http://#unknown'
 
-    @property
+    @Lazy
     def errorUrl(self):
         try:
             return self.context.REQUEST.get('URL', 'http://#unknown')
         except:
             return 'http://#unknown'
 
-    @property
+    @Lazy
     def internalRequest(self):
         refererHost = urlparse(self.refererUrl)[1]
         siteHost = urlparse(self.errorUrl)[1]
         retval = (refererHost == siteHost) and (not self.directRequest)
-
         return retval
 
-    @property
+    @Lazy
     def externalRequest(self):
         retval = (not self.internalRequest) and (not self.directRequest)
-
         return retval
 
-    @property
+    @Lazy
     def directRequest(self):
         retval = self.refererUrl in (None, '')
-
         return retval
 
-    @property
+    @Lazy
     def searchRequest(self):
         retval = False
         refererHost = urlparse(self.refererUrl)[1]
@@ -76,12 +75,16 @@ class BaseError(object):
                 break
         return retval
 
-    def update(self):
-        self.response.setStatus(self.status)
-
     def supportMessage(self):
         # Should return a support message suitable for email in the subclass.
         raise NotImplementedError
+
+
+class BaseError(CoreError):
+    '''The base error class, that is used by Grok'''
+
+    def update(self):
+        self.response.setStatus(self.status)
 
     def render(self):
         # force the return type to be text/html, just in case
@@ -90,21 +93,6 @@ class BaseError(object):
 
 
 # This is still used by a couple of things, but relates to the old system
-class BaseErrorPage(BrowserView):
+class BaseErrorPage(BrowserView, CoreError):
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
-        self.__siteInfo = self.__supportEmail = None
-
-    @property
-    def siteInfo(self):
-        if self.__siteInfo is None:
-            self.__siteInfo = createObject('groupserver.SiteInfo',
-                self.context)
-        return self.__siteInfo
-
-    @property
-    def supportEmail(self):
-        if self.__supportEmail is None:
-            self.__supportEmail = get_support_email(self.context,
-                                    self.siteInfo.id)
-        return self.__supportEmail
--- gs/errormesg/notfound.py
+++ gs/errormesg/notfound.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 *-*
 from urllib import quote
+from urlparse import urlparse
 from five import grok
 from zope.publisher.interfaces import INotFound
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
-from baseerror import BaseError
+from baseerror import BaseError, BaseErrorPage
+
+MESSAGE = u'''Hi! I saw a Not Found (404) page when I went to
+{url}
+
+I expected to see...
+
+-----
+Technical details:
+Code: 404
+URL: {url}
+Referer: {referer}
+'''
 
 
 class NotFound(BaseError, grok.View):
@@ -13,20 +26,29 @@ class NotFound(BaseError, grok.View):
     status = 404
 
     def supportMessage(self):
-        m = u'''Hi! I saw a Not Found (404) page when I went to
-%(url)s
-
-I expected to see...
-
-
------
-Technical details:
-Code: 404
-URL: %(url)s
-Referer: %(referer)s
-''' % {'url': self.errorUrl,
-       'referer': self.refererUrl}
-
+        m = MESSAGE.format(url=self.errorUrl, referer=self.refererUrl)
         retval = quote(m)
+        return retval
 
+
+class NotFoundZope2(BaseErrorPage):
+    '''The Zope2 version of the error page.'''
+    status = 404
+
+    def __init__(self, context, request):
+        super(NotFoundZope2, self).__init__(context, request)
+
+        self.requested = request.form.get('q', '')
+
+        r = request.form.get('r', '')
+        self.referer = type(r) == list and r[0] or r
+        self.refererUrl = urlparse(self.referer)
+
+        contentType = 'text/html; charset=UTF-8'
+        self.request.response.setHeader('Content-Type', contentType)
+        self.request.response.setStatus(self.status, lock=True)
+
+    def supportMessage(self):
+        m = MESSAGE.format(url=self.requested, referer=self.refererUrl)
+        retval = quote(m)
         return retval
